655b6e1ab461c3ca719f7f2f081d4703828f3303
PAT CAMP <PATS0825@MSN.COM>
2018-08-15 13:11:36 -0400

    added ability to sign up as admin to demonstrate additional function

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+
+
+
+
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :admin, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :admin, :password, :password_confirmation)
+  end
+end
